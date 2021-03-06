87588c1a5db4632f285bc072b4d2228fa4b8eaa1
sergun-36 <sergun_36@mail.ru>
2021-03-06 19:14:03 +0300

    added logging and get_dynamic

--- logic/Courses.py
+++ logic/Courses.py
@@ -1,10 +1,19 @@
 import requests
 import settings
 import datetime
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger=logging.getLogger(__name__)
 
 class Courses():
-	rb_bank_root_url=settings.rb_bank_root_url
+	rb_bank_root_url=settings.RB_BANK_ROOT_URL
 
+
+
+	def set_country(self, country=""):
+		self.country=country
 
 	"""
 	get rates on date(default today)  for all currenсies
@@ -13,47 +22,95 @@ class Courses():
 	def get_currenсies_on_date(self, date=datetime.date.today()):
 		url=f"{self.rb_bank_root_url}?ondate={date}&periodicity=0"
 		try:
-			response=requests.get(url)
-			currencies_on_date=response.json()
-			return currencies_on_date
+			res=requests.get(url)
+			status=res.status_code
+			if status in settings.OK_CODES:
+				result={"ok" : True,
+						"currencies_on_date" : res.json()}
+				logger.info(f"Request on date {date} is successfull. Status {status}")
+			else:
+				result={"ok" : False,
+						"error_message" : f"Request on date {date} is failed. Status {status}"}
+				logger.warning(f"Request on date {date} is failed. Status {status}")
+			return result
 		except Exception as ex:
-			raise Exception(f"Error. {response}. Exception - {ex}")
+			logger.error(f"Error - {res}. Exception - {ex}")
+			raise Exception(f"Error - {res}. Exception - {ex}")
 
 
 	"""
 	get rates all currencies on date(date format - "yyyy-mm-dd") and select needed by money abbreviation
 	return dict with single currenсy data.
 	"""
-	def currencies_on_date_by_abbr(self, money_abbr="", date=datetime.date.today()):
+	def get_currencies_on_date_by_abbr(self, money_abbr="", date=datetime.date.today()):
 		money_abbr=money_abbr.upper()
-		currencies_on_date=self.get_currenсies_on_date(date=date)
-
-		for curr in currencies_on_date:
-			if curr["Cur_Abbreviation"]==money_abbr:
-				return curr
+		currs_on_date=self.get_currenсies_on_date(date=date)
+		
+		if currs_on_date["ok"]:
+			for curr in currs_on_date["currencies_on_date"]:
+				if curr["Cur_Abbreviation"]== money_abbr:
+					logger.info(f"Data for \'{money_abbr}\' is founded successfull")
+					return {"ok" : True,
+							"curr" : curr}
+				
+			logger.warning(f"Such currency as \'{money_abbr}\' in not founded")
+			return {"ok" : False,
+					"error_message" : f"Such currency as \'{money_abbr}\' in not founded"}
+		else:
+			logger.warning(currs_on_date["error_message"])
+			return  {"ok" : False,
+					"error_message" : f"Such currency as \'{money_abbr}\' in not founded"}
 
 
 	"""
 	get cur ID of currency by money abbreviation.
 	"""
-	def get_cur_id_by_abbr(self, money_abbr=""):
-		curr=self.currencies_on_date_by_abbr(money_abbr=money_abbr)
-		return curr["Cur_ID"]
-
+	def get_curr_id_by_abbr(self, money_abbr=""):
+		curr_data=self.get_currencies_on_date_by_abbr(money_abbr=money_abbr)
+		if curr_data["ok"]:
+			curr_id=curr_data["curr"]["Cur_ID"]
+			logger.info(f"cur_id {curr_id} for \'{money_abbr}\' get successfull")
+			return curr_id
+		else:
+			logger.warning(f"cur_id did not get for \'{money_abbr}\'")
 
 	"""
 	get dynamics rate of currency for period(from today) by money abbreviation 
 	"""
 	def get_dynamic_rate_by_perid_money_abbr(self, money_abbr="", period=3):
-		cur_id=get_cur_id_by_abbr(money_abbr=money_abbr)
+		cur_id=self.get_curr_id_by_abbr(money_abbr=money_abbr)
+		if cur_id:
+			end_date=datetime.date.today()
+			start_date=datetime.datetime.now()-datetime.timedelta(days=period+1)#.date()
+			url=f"{self.rb_bank_root_url}/Dynamics/{cur_id}?startDate={start_date}&endDate={end_date}"
 
-		
+			try:
+				res=requests.get(url)
+				status=res.status_code
 
+				if status in settings.OK_CODES:
+					result={"ok" : True,
+							"curr_dynamic" : res.json()}
+					logger.info(f"Currency dynamic \'{money_abbr}\' for {period} days got successfull. Status {status}")
+				else:
+					result={"ok" : False,
+							"error_message" : f"Currency dynamic did not get. Status {status}"}
+					logger.warning(f"Currency dynamic did not get. Status {status}")
+				
+			except Exception as ex:
+				logger.warning(f"Request of currency dynamic was failed. Error {ex}")
+				raise Exception(f"Request of currency dynamic was failed. Error {ex}") 
+
+		else:
+			logger.warning(f"Cur_ID \'{money_abbr}\' is not founded")
+			result={"ok" : False,
+					"error_message" : f"Cur_ID \'{money_abbr}\' is not founded"}
+		return result
 
 		
 
 
 courses=Courses()
-print(courses.get_cur_id_by_abbr("USD"))
+print(courses.get_dynamic_rate_by_perid_money_abbr("uah"))
 
 
--- logic/settings.py
+++ logic/settings.py
@@ -1,5 +1,7 @@
 bot_token="1314411224:AAGxkdEIoxb6VgnMjh0P-0TKpwTLZMvbJ1I"
 teleg_root_url="https://api.telegram.org/bot"
 
-rb_bank_root_url="https://www.nbrb.by/api/exrates/rates"
+OK_CODES=(200, 201, 202)
+
+RB_BANK_ROOT_URL="https://www.nbrb.by/api/exrates/rates"
 #rb_bank_url_dynamics=f"{rb_bank_root_url}/Dynamics/{Cur_Id}?startDate={2016-6-1}&endDate={2016-6-30}"
