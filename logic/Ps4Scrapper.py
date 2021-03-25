22762dfb27583b19bbd670a35a291544089265a2
sergun-36 <sergun_36@mail.ru>
2021-03-25 19:04:03 +0300

    try parse

--- logic/Ps4Scrapper.py
+++ logic/Ps4Scrapper.py
@@ -1,4 +1,4 @@
-import bs4
+from bs4 import BeautifulSoup
 import requests
 import settings
 
@@ -18,16 +18,26 @@ class ParsesPs4():
 				all_disc_text = {"ok" : False,
 								"error_message" : f"Response code: {status_code}"}
 				logger.info(f"Request on ps4 is failed. Status is {status_code}")
-				
+
 		except Exception as e:
 			logger.error(f"Request on ps4 is failed. Error - {e}")
 			raise Exception(f"Request on ps4 is failed. Error - {e}")
 
-		return result
+		return all_disc_text
+
+	def get_title(self, all_text=""):
+		soup = BeautifulSoup(all_text, "lxml")
+		links = soup.find_all('a')
+		text=[]
+		for link in links:
+			text.append(link['class'])#data-telemetry-meta
+
+		return text
 
 
 
 
 pers=ParsesPs4()
-xml = pers.get_discount_row()
+all_text = pers.get_discount_row()
+xml = pers.get_title(all_text)
 print(xml)
