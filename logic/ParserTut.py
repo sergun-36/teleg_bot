75e3ee34587fb26e1a34d3c49df895e41caf9d7b
sergun-36 <sergun_36@mail.ru>
2021-03-27 16:33:25 +0300

    added get movies info, shall add loop, format time

--- logic/ParserTut.py
+++ logic/ParserTut.py
@@ -29,27 +29,80 @@ class ParserTut():
 
 		return movies_html
 
-	def get_movies_info(self, movies_html):
+
+	def get_comun_divs(self, movies_html):
 		soup_html = BeautifulSoup(movies_html, "lxml")
-		links = soup_html.find_all("a", class_="header__link", title= True)
-		for link in links:
-			if link.string:
-				print(link.string)
+		divs = soup_html.find_all("div", class_="a-event-i")
+		logger.info(f"Printim links")
+		for div in divs:
+			#print(div)
+			return div
+			pass
+		return divs
+		
+
+
+	def get_cinemas_info(self, div):
+		cinemas_tag = div.find_all("a", class_="header__link", title= True)
+		for cinema in cinemas_tag:
+			if cinema.string:
+				print(cinema.string)
+				return cinema.string
 		logger.info(f"Printim links")
 
 
-	def get_links_info(self, movies_html):
-		soup_html = BeautifulSoup(movies_html, "lxml")
-		links = soup_html.find_all("div", class_="a-event-header")
-		for link in links:
-			link = link.next_sibling
-			aes = link.find_all('a')
-			for a in aes:
-				print(a.string)
-			
-		logger.info(f"Printim links")	
-		
+	def get_films_info(self, div):
+		films_tag = div.find_all('span', itemprop="name")
+		films = []
+		for film in films_tag:
+			if film.string:
+				films.append(film.string)
+				#print(film.string)
+		return films
 
+	def get_time_film(self, film):
+		times = []
+
+		times_tag = film.parent.parent.parent.find_all('time')
+		for time in times_tag:
+			print(time["datetime"])
+			times.append(time["datetime"])
+
+		return times
+		logger.info(f"Printim time")
+
+
+	def get_movies_info(self, movies_html):
+		movies={}
+		div = self.get_comun_divs(movies_html)
+		#loop for div in divs
+		cinema = self.get_cinemas_info(div)
+		movies[str(cinema)]={}
+		films = self.get_films_info(div)
+		for film in films:
+			times = self.get_time_film(film)
+			movies[str(cinema)][str(film)]= times
+		return movies
+
+
+
+
+
+
+
+	
 parser = ParserTut()
 movies_html = parser.get_all_info_movies()
-links=parser.get_links_info(movies_html)
+
+movies = parser.get_movies_info(movies_html)
+print(movies)
+'''
+div = parser.get_comun_divs(movies_html)
+films = parser.get_films_info(div)
+for film in films:
+	print(film.parent)
+	times = parser.get_time_film(film)
+print(times)
+#cinemas = parser.get_movies_info(div)
+#links=parser.get_links_info(movies_html)
+'''
