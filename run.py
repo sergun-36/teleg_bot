ccafd368e89db4b32d8706169de86b323cbcc6cb
sergun-36 <sergun_36@mail.ru>
2022-09-23 19:28:46 +0300

    added app to logic from run.py

--- run.py
+++ run.py
@@ -1,19 +1,4 @@
-from  logic.TelegBot import TelegBot
-from flask import Flask, request
-from logic.settings import bot_token
-
-secret = bot_token
-bot=TelegBot()
-bot.webhook()
-
-app = Flask(__name__)
-
-@app.route(f"/{secret}", methods=["POST"])
-def webhook():
-	#bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-	updates = request.json
-	bot.handling_message(updates)
-	return "ok", 200
+from logic.app import app
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
